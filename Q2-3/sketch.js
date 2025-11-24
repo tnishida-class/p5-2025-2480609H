51e96b95cc22da8092592a2a25ad1b99d4f5f886
2480609H <maimai.k0251@gmail.com>
2025-11-24 19:43:43 +0900

    finalwork

--- Q2-3/sketch.js
+++ Q2-3/sketch.js
@@ -10,6 +10,12 @@ function setup() {
   // 縞を描く
   for(let i = 0; i < 9; i++){
     // BLANK[1] ヒント：縞の色を交互に変えるには2で割った余りを使おう
+    if((i%2)==0){
+      fill(blue)
+    }
+    else{
+      fill(255)
+    }
     rect(0, i * d, width, (i + 1) * d);
   }
 
@@ -19,5 +25,6 @@ function setup() {
   rect(0, 0, size, size);
   fill(255);
   rect(d * 2, 0, d, size);
-  // BLANK[2] 十字を完成させよう
+  fill(255);
+  rect(0,d*2,size,d);// BLANK[2] 十字を完成させよう
 }
